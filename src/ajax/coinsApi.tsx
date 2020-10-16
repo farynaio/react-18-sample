2fa40228a4aa8c6a8155943be9fb24bbb45bfc6e
sanczol <adamfaryna@gmail.com>
2020-10-16 11:49:17 +0100

    Fetch coins added server error fallback and tests

--- src/ajax/coinsApi.tsx
+++ src/ajax/coinsApi.tsx
@@ -5,22 +5,29 @@ const CRYPTO_COMPARE_API_URL = "https://min-api.cryptocompare.com/data/";
 export const fetchCoins = async (currency: string, cryptoCoins: Array<string>): Promise<Array<Coin>> => {
     const coinsParam = cryptoCoins.join(",");
     const url = `${CRYPTO_COMPARE_API_URL}pricemultifull?fsyms=${coinsParam}&tsyms=${currency}`;
-    const res = await fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-    });
 
-    let data: any = await res.json();
-    data = data["RAW"];
+    try {
+        const res = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
 
-    const result: Array<Coin> = [];
+        let data: any = await res.json();
+        data = data["RAW"];
 
-    for (let elem in data) {
-        const coin = new Coin(elem, data[elem][currency]["PRICE"], data[elem][currency]["OPENDAY"]);
-        result.push(coin);
+        const result: Array<Coin> = [];
+
+        for (let elem in data) {
+            const coin = new Coin(elem, data[elem][currency]["PRICE"], data[elem][currency]["OPENDAY"]);
+            result.push(coin);
+        }
+
+        return result;
+
+    } catch (e) {
+        console.error("Fetch coins data failed!");
+        throw e;
     }
-
-    return result;
 };
